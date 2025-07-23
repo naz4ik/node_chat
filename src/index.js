2516558231ad1147b99a4b15000af8a3069fbf52
Nazar <154551346+naz4ik@users.noreply.github.com>
2025-07-23 12:29:59 +0200

    add task solution

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 'use strict';
+import express from 'express';
+import cors from 'cors';
+import { userRouter } from './routes/user.router.js';
+import { messageRouter } from './routes/message.router.js';
+import { roomRouter } from './routes/room.routes.js';
+import { WebSocketServer } from 'ws';
+import { messageEmitter } from './controllers/message.controller.js';
+
+const PORT = process.env.PORT || 3005;
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
+
+app.use('/user', userRouter);
+app.use('/rooms', roomRouter);
+app.use('/rooms', messageRouter);
+
+app.get('/', (req, res) => {
+  res.send('Server is OK');
+});
+
+const server = app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('server is running');
+});
+
+const wss = new WebSocketServer({ server });
+
+messageEmitter.on('message', (message) => {
+  for (const client of wss.clients) {
+    client.send(JSON.stringify(message));
+  }
+});
---
+++ src/models/Message.route.js
@@ -0,0 +1,18 @@
+import { DataTypes } from 'sequelize';
+import { client } from '../db.js';
+
+export const Message = client.define('Message', {
+  text: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    allowNull: false,
+  },
+});
